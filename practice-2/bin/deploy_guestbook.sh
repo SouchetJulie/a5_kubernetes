eeeadc7855eb9ee153b4a7e93643f749782bf18c
Julie Souchet <90184650+SouchetJulie@users.noreply.github.com>
2023-02-14 15:46:35 +0100

    create services first

--- practice-2/bin/deploy_guestbook.sh
+++ practice-2/bin/deploy_guestbook.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 echo "Deploying Redis leader & followers..."
-kubectl apply -f ../redis/redis-leader-deployment.yaml
 kubectl apply -f ../redis/redis-leader-service.yaml
-kubectl apply -f ../redis/redis-follower-deployment.yaml
 kubectl apply -f ../redis/redis-follower-service.yaml
+kubectl apply -f ../redis/redis-leader-deployment.yaml
+kubectl apply -f ../redis/redis-follower-deployment.yaml
 
 echo "Deploying Frontend..."
-kubectl apply -f ../front/front-deployment.yaml
 kubectl apply -f ../front/front-service.yaml
+kubectl apply -f ../front/front-deployment.yaml
 
 echo "Frontend is available at http://localhost:8080"
 kubectl port-forward svc/frontend 8080:80 -n front
